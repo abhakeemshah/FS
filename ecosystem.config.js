27b04856cf7f054cc7e9e7e0a0dc30c1fe97f9eb
Abduil Hakeem Shah <abdulhakeemshah962@gmail.com>
2026-06-16 20:31:37 +0500

    fix: dynamically bind Next.js start script to Hostinger environment port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,7 @@
   "log_date_format": "YYYY-MM-DD HH:mm:ss Z",
   "merge_logs": true,
   "env": {
-    "NODE_ENV": "production",
-    "PORT": 3000
+    "NODE_ENV": "production"
   },
   "watch": false,
   "ignore_watch": [
